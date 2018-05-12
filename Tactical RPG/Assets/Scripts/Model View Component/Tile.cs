c9f1b62a71db7b7370974c653632066e0107325f
Jonathan L <jlemanski1@gmail.com>
2018-05-12 02:09:18 -0600

    Tile changes
    - Added Tile.cs
        - Basic Tile methods to be used for later map creation

---
+++ Tactical RPG/Assets/Scripts/Model View Component/Tile.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    // 4 steps = width of tile
+    public const float stepHeight = 0.25f;
+
+    public Point pos;
+    public int height;
+
+    // Center of the tile
+    public Vector3 center { get { return new Vector3(pos.x, height * stepHeight, pos.y); } }
+
+
+    /// <summary>
+    /// Visually reflect any new values
+    /// Updates position & scale
+    /// </summary>
+    private void UpdateTile() {
+        transform.localPosition = new Vector3(pos.x, height * stepHeight / 2f, pos.y);
+        transform.localScale = new Vector3(1, height * stepHeight, 1);
+    }
+
+    /// <summary>
+    /// Increase tile size
+    /// </summary>
+    public void Grow() {
+        height++;
+        UpdateTile();
+    }
+
+    /// <summary>
+    /// Decrease tile size
+    /// </summary>
+    public void Shrink() {
+        height--;
+        UpdateTile();
+    }
+
+    /// <summary>
+    /// Loads Tile position and height for persisting tile data
+    /// </summary>
+    /// <param name="p">position</param>
+    /// <param name="h">height</param>
+    public void Load(Point p, int h) {
+        pos = p;
+        height = h;
+        UpdateTile();
+    }
+
+    /// <summary>
+    /// Loads Tile position through a Vector3
+    /// </summary>
+    /// <param name="v">Vector3 containing tile position</param>
+    public void Load(Vector3 v) {
+        Load(new Point((int)v.x, (int)v.z), (int)v.y);
+    }
+
+}
