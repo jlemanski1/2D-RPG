5eeeabbb9fa2ad152524a501cf6a882efac6ebac
Jonathan L <jlemanski1@gmail.com>
2018-05-12 12:20:31 -0600

    Level Creator
    - Able to build levels at edit-time, save & load them for later use

---
+++ Tactical RPG/Assets/Scripts/Editor/LevelCreatorInspector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Custom Editor to allow for creation of levels during edit-time
+/// </summary>
+
+[CustomEditor(typeof(LevelCreator))]
+public class LevelCreatorInspector : Editor {
+
+    // Selected object
+    public LevelCreator current {
+        get { return (LevelCreator)target; }
+    }
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Clear"))
+            current.ClearLevel();
+
+        if (GUILayout.Button("Grow"))
+            current.Grow();
+
+        if (GUILayout.Button("Shirnk"))
+            current.Shrink();
+
+        if (GUILayout.Button("Grow Area"))
+            current.GrowArea();
+
+        if (GUILayout.Button("Shrink Area"))
+            current.ShrinkArea();
+
+        if (GUILayout.Button("Save"))
+            current.SaveLevel();
+
+        if (GUILayout.Button("Load"))
+            current.LoadLevel();
+
+        if (GUI.changed)
+            current.UpdateMarker();
+    }
+}
