1646db2418123b799e34c73e55671d5ba24b7e0e
Jonathan L <jlemanski1@gmail.com>
2018-05-13 01:23:00 -0600

    Initial Battle State & test Scene
    - Added InitBattleState
    - Added MoveTargetState
    - Added new scene to test out everything called Battle

---
+++ Tactical RPG/Assets/Scripts/Controller/BattleStates/InitBattleState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// State that the BattleController will start with. Creates and loads everything that's 
+/// necessary, then triggers the next state when its complete
+/// </summary>
+public class InitBattleState : BattleState {
+
+    public override void Enter() {
+        base.Enter();   // Add Listeners
+        StartCoroutine(Init());
+    }
+
+    IEnumerator Init() {
+        level.Load(levelData);
+        Point p = new Point((int)levelData.tiles[0].x, (int)levelData.tiles[0].z);
+        SelectTile(p);
+        yield return null;
+        owner.ChangeState<MoveTargetState>();
+    }
+}
