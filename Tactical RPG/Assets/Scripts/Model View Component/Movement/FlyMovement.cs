d03a882a293e77b76533a372eae35db311188efb
Jonathan L <jlemanski1@gmail.com>
2018-05-17 15:35:52 -0600

    Movement Subclasses
    - Added WalkMovement for walking units (path limits)
    - Added FlyMovement for flying units (no path restrictions except distance)
    - Added TeleportMovement for units that teleport to target tile

---
+++ Tactical RPG/Assets/Scripts/Model View Component/Movement/FlyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Movement class for units that fly, and are not limited by objects or enemies
+/// blocking the path
+/// </summary>
+public class FlyMovement : Movement {
+
+    /// <summary>
+    /// Override of the Traverse Animation for flying units. Takes off, flys to tile, then lands.
+    /// </summary>
+    public override IEnumerator Traverse(Tile tile) {
+        // Distance between that start tile and target tile
+        float dist = Mathf.Sqrt(Mathf.Pow(tile.pos.x - unit.tile.pos.x, 2) +
+            Mathf.Pow(tile.pos.y - unit.tile.pos.y, 2));
+        unit.Place(tile);
+
+        // Fly high enough not to clip through any ground tiles
+        float y = Tile.stepHeight * 10;
+        float duration = (y - jumper.position.y) * 0.5f;
+        Tweener tweener = jumper.MoveToLocal(new Vector3(0, y, 0), duration,
+            EasingEquations.EaseInOutQuad);
+        while (tweener != null)
+            yield return null;
+
+        // Turn to face the general target direction
+        Directions dir;
+        Vector3 toTile = (tile.center - transform.position);
+        if (Mathf.Abs(toTile.x) > Mathf.Abs(toTile.z))
+            dir = toTile.x > 0 ? Directions.East : Directions.West;
+        else
+            dir = toTile.z > 0 ? Directions.North : Directions.South;
+        yield return StartCoroutine(Turn(dir));
+
+        // Move to the correct position
+        duration = dist * 0.5f;
+        tweener = transform.MoveTo(tile.center, duration, EasingEquations.EaseInOutQuad);
+        while (tweener != null)
+            yield return null;
+
+        // Land back on the ground
+        duration = (y - tile.center.y) * 0.5f;
+        tweener = jumper.MoveToLocal(Vector3.zero, 0.5f, EasingEquations.EaseInOutQuad);
+        while (tweener != null)
+            yield return null;
+    }
+
+}
---
+++ Tactical RPG/Assets/Scripts/Model View Component/Movement/WalkMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Movement subclass for units that can walk and are limited by enemies, objects,
+/// or obstacles on tiles.
+/// </summary>
+public class WalkMovement : Movement {
+
+    /// <summary>
+    /// Retain the basic distance check of ExpandSearch while making sure that
+    /// the unit can jump the height of the tile and that the tile is not occupied
+    /// </summary>
+    protected override bool ExpandSearch(Tile from, Tile to) {
+        // Skip if unit can jump that high
+        if ((Mathf.Abs(from.height - to.height) > jumpHeight))
+            return false;
+
+        // Skip if tile is occupied by an enemy or misc object
+        if (to.content != null)
+            return false;
+
+        return base.ExpandSearch(from, to);
+    }
+
+
+    /// <summary>
+    /// Animate movement along a path (implemented using nested coroutines)
+    /// </summary>
+    public override IEnumerator Traverse(Tile tile) {
+        unit.Place(tile);
+
+        // Build a list of waypoints from the unit's starting tile to the destination tile
+        List<Tile> targets = new List<Tile>();
+        while (tile != null) {
+            targets.Insert(0, tile);
+            tile = tile.prev;
+        }
+
+        // Move to each way point in succession
+        for (int i = 1; i < targets.Count; i++) {
+            Tile from = targets[i - 1];
+            Tile to = targets[i];
+
+            Directions dir = from.GetDirection(to);
+            if (unit.dir != dir)
+                yield return StartCoroutine(Turn(dir));
+
+            if (from.height == to.height)
+                yield return StartCoroutine(Walk(to));
+            else
+                yield return StartCoroutine(Jump(to));
+        }
+        yield return null;
+    }
+
+
+    /// <summary>
+    /// Animate walking movement
+    /// </summary>
+    /// <param name="target">Tile to walk to</param>
+    IEnumerator Walk(Tile target) {
+        Tweener tweener = transform.MoveTo(target.center, 0.5f, EasingEquations.Linear);
+        while (tweener != null)
+            yield return null;
+    }
+
+
+    /// <summary>
+    /// Animate jumping movement
+    /// </summary>
+    /// <param name="to">Tile to jump to</param>
+    IEnumerator Jump(Tile to) {
+        Tweener tweener = transform.MoveTo(to.center, 0.5f, EasingEquations.Linear);
+        Tweener tweener2 = jumper.MoveToLocal(new Vector3(0, Tile.stepHeight * 2f, 0),
+            tweener.easingControl.duration / 2f, EasingEquations.EaseOutQuad);
+        tweener2.easingControl.loopCount = 1;
+        tweener2.easingControl.loopType = EasingControl.LoopType.PingPong;
+
+        while (tweener != null)
+            yield return null;
+    }
+}
