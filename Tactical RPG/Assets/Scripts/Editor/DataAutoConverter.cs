79275e43f74df39ab8d99d3949857dbfe6fc710e
Jonathan L <jlemanski1@gmail.com>
2018-05-24 15:31:53 -0600

    CSV asset loading
    - Added loading & autoconverting of data spreadsheets to ScriptableObjects

---
+++ Tactical RPG/Assets/Scripts/Editor/DataAutoConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Automatically copies the data from the spreadsheets to a scriptable object
+/// Listens for changes in the data spreadsheets, and updates as necessary
+/// </summary>
+public class DataAutoConverter : AssetPostprocessor {
+
+    static Dictionary<string, Action> parsers;
+
+    /* To load/Unload assets at runtime
+     * 
+     * void Start() {
+     * EnemyData test = Resources.Load<EnemyData>("TEST");      // Load
+     * Debug.Log(test.hp);
+     * test = null;
+     * Resources.UnloadUnusedAssets();      // Unload
+     * }
+     */
+
+
+    /// <summary>
+    /// Link the names of data spreadsheets to an Action (Delegate that performs a task)
+    /// </summary>
+    static DataAutoConverter() {
+        parsers = new Dictionary<string, Action>();
+        parsers.Add("Enemies_test.csv", ParseEnemies);
+    }
+
+
+    /// <summary>
+    /// Loop through changed files and if any of them are in the parser dictionary,
+    /// parse the spreadsheet with the associated action
+    /// </summary>
+    static void OnPostProcessAllAssets(string[] importedAssets, string[] deletedAssets,
+        string[] movedAssets, string[] movedFromAssetPaths) {
+        for (int i = 0; i < importedAssets.Length; i++) {
+            string fileName = Path.GetFileName(importedAssets[i]);
+            if (parsers.ContainsKey(fileName))
+                parsers[fileName]();
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+
+    /// <summary>
+    /// Parse the data and create the necessary ScriptableObject in the Data/Resources/ folder
+    /// </summary>
+    static void ParseEnemies() {
+        string filePath = Application.dataPath + "/Data/Enemies_test.csv";
+        if (!File.Exists(filePath)) {
+            Debug.LogError("ERROR! Missing Enemy Data: " + filePath);
+            return;
+        }
+
+        string[] readText = File.ReadAllLines("Assets/Data/Enemies_test.csv");
+        filePath = "Assets/Data/Resources/";
+        for (int i = 1; i < readText.Length; i++) {
+            EnemyData enemyData = ScriptableObject.CreateInstance<EnemyData>();
+            enemyData.Load(readText[1]);
+            string fileName = string.Format("{0} {1}.asset", filePath, enemyData.name);
+            AssetDatabase.CreateAsset(enemyData, fileName);
+        }
+    }
+}
---
+++ Tactical RPG/Assets/Scripts/Model View Component/EnemyData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Loads and holds an entry of the enemy data spreadsheet
+/// </summary>
+public class EnemyData : ScriptableObject {
+
+    public int hp;          // Health points
+    public int hitCount;    // Amount of times it can attack
+    public int damage;      // Attack damage
+    public int agility;     // Agility / Weight (speed/heft)
+    public int xp;          // XP reward for killing
+    public int gold;        // Gold reward for killing
+
+
+    /// <summary>
+    /// Loads enemy data file and saves to appropriate vars
+    /// </summary>
+    public void Load(string line) {
+        string[] elements = line.Split(',');
+        name = elements[0];
+        hp = Convert.ToInt32(elements[1]);
+        hitCount = Convert.ToInt32(elements[2]);
+        damage = Convert.ToInt32(elements[3]);
+        agility = Convert.ToInt32(elements[4]);
+        xp = Convert.ToInt32(elements[5]);
+        gold = Convert.ToInt32(elements[6]);
+    }
+}
